f95fe0af3aa55b42cdfe299810cbf90287465509
MTalano <Im@MathewTalano.com>
2018-03-14 09:12:04 -0500

    Updates in the last two weeks.

--- MaintenanceTracker/AirFilterOptionsForm.Designer.cs
+++ MaintenanceTracker/AirFilterOptionsForm.Designer.cs
@@ -28,19 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AirFilterOptionsForm));
+            this.engAirFilter = new System.Windows.Forms.Button();
+            this.cabAirFilter = new System.Windows.Forms.Button();
+            this.engAirFilterLB = new System.Windows.Forms.Label();
+            this.cabAirFilterLB = new System.Windows.Forms.Label();
+            this.engAirFilterTB = new System.Windows.Forms.TrackBar();
+            this.cabAirFilterTB = new System.Windows.Forms.TrackBar();
+            this.resetBTTN = new System.Windows.Forms.Button();
+            this.exitBTTN = new System.Windows.Forms.Button();
+            this.engAirFilterTbLb = new System.Windows.Forms.Label();
+            this.cabAirFilterTbLb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.engAirFilterTB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cabAirFilterTB)).BeginInit();
             this.SuspendLayout();
+            // 
+            // engAirFilter
+            // 
+            this.engAirFilter.Location = new System.Drawing.Point(45, 543);
+            this.engAirFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.engAirFilter.Name = "engAirFilter";
+            this.engAirFilter.Size = new System.Drawing.Size(176, 176);
+            this.engAirFilter.TabIndex = 0;
+            this.engAirFilter.Text = "Engine Air Filter";
+            this.engAirFilter.UseVisualStyleBackColor = true;
+            this.engAirFilter.Click += new System.EventHandler(this.EngAirFilter_Click);
+            // 
+            // cabAirFilter
+            // 
+            this.cabAirFilter.Location = new System.Drawing.Point(309, 543);
+            this.cabAirFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.cabAirFilter.Name = "cabAirFilter";
+            this.cabAirFilter.Size = new System.Drawing.Size(176, 176);
+            this.cabAirFilter.TabIndex = 1;
+            this.cabAirFilter.Text = "Cabin Air Filter";
+            this.cabAirFilter.UseVisualStyleBackColor = true;
+            this.cabAirFilter.Click += new System.EventHandler(this.CabAirFilter_Click);
+            // 
+            // engAirFilterLB
+            // 
+            this.engAirFilterLB.AutoSize = true;
+            this.engAirFilterLB.Location = new System.Drawing.Point(24, 17);
+            this.engAirFilterLB.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.engAirFilterLB.Name = "engAirFilterLB";
+            this.engAirFilterLB.Size = new System.Drawing.Size(0, 25);
+            this.engAirFilterLB.TabIndex = 4;
+            // 
+            // cabAirFilterLB
+            // 
+            this.cabAirFilterLB.AutoSize = true;
+            this.cabAirFilterLB.Location = new System.Drawing.Point(24, 175);
+            this.cabAirFilterLB.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.cabAirFilterLB.Name = "cabAirFilterLB";
+            this.cabAirFilterLB.Size = new System.Drawing.Size(0, 25);
+            this.cabAirFilterLB.TabIndex = 5;
+            // 
+            // engAirFilterTB
+            // 
+            this.engAirFilterTB.Location = new System.Drawing.Point(45, 430);
+            this.engAirFilterTB.Name = "engAirFilterTB";
+            this.engAirFilterTB.Size = new System.Drawing.Size(176, 90);
+            this.engAirFilterTB.TabIndex = 6;
+            // 
+            // cabAirFilterTB
+            // 
+            this.cabAirFilterTB.Location = new System.Drawing.Point(309, 430);
+            this.cabAirFilterTB.Name = "cabAirFilterTB";
+            this.cabAirFilterTB.Size = new System.Drawing.Size(176, 90);
+            this.cabAirFilterTB.TabIndex = 7;
+            // 
+            // resetBTTN
+            // 
+            this.resetBTTN.Location = new System.Drawing.Point(45, 738);
+            this.resetBTTN.Margin = new System.Windows.Forms.Padding(4);
+            this.resetBTTN.Name = "resetBTTN";
+            this.resetBTTN.Size = new System.Drawing.Size(176, 56);
+            this.resetBTTN.TabIndex = 9;
+            this.resetBTTN.Text = "Reset";
+            this.resetBTTN.UseVisualStyleBackColor = true;
+            this.resetBTTN.Click += new System.EventHandler(this.ResetBTTN_Click);
+            // 
+            // exitBTTN
+            // 
+            this.exitBTTN.Location = new System.Drawing.Point(309, 738);
+            this.exitBTTN.Margin = new System.Windows.Forms.Padding(4);
+            this.exitBTTN.Name = "exitBTTN";
+            this.exitBTTN.Size = new System.Drawing.Size(176, 56);
+            this.exitBTTN.TabIndex = 8;
+            this.exitBTTN.Text = "Exit";
+            this.exitBTTN.UseVisualStyleBackColor = true;
+            this.exitBTTN.Click += new System.EventHandler(this.ExitBTTN_Click);
+            // 
+            // engAirFilterTbLb
+            // 
+            this.engAirFilterTbLb.AutoSize = true;
+            this.engAirFilterTbLb.Location = new System.Drawing.Point(40, 495);
+            this.engAirFilterTbLb.Name = "engAirFilterTbLb";
+            this.engAirFilterTbLb.Size = new System.Drawing.Size(0, 25);
+            this.engAirFilterTbLb.TabIndex = 10;
+            // 
+            // cabAirFilterTbLb
+            // 
+            this.cabAirFilterTbLb.AutoSize = true;
+            this.cabAirFilterTbLb.Location = new System.Drawing.Point(304, 495);
+            this.cabAirFilterTbLb.Name = "cabAirFilterTbLb";
+            this.cabAirFilterTbLb.Size = new System.Drawing.Size(0, 25);
+            this.cabAirFilterTbLb.TabIndex = 11;
             // 
             // AirFilterOptionsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 667);
+            this.ClientSize = new System.Drawing.Size(544, 835);
+            this.Controls.Add(this.cabAirFilterTbLb);
+            this.Controls.Add(this.engAirFilterTbLb);
+            this.Controls.Add(this.resetBTTN);
+            this.Controls.Add(this.exitBTTN);
+            this.Controls.Add(this.cabAirFilterTB);
+            this.Controls.Add(this.engAirFilterTB);
+            this.Controls.Add(this.cabAirFilterLB);
+            this.Controls.Add(this.engAirFilterLB);
+            this.Controls.Add(this.cabAirFilter);
+            this.Controls.Add(this.engAirFilter);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AirFilterOptionsForm";
-            this.Text = "AirFileterOptionsForm";
+            this.Text = "Air Filter";
+            ((System.ComponentModel.ISupportInitialize)(this.engAirFilterTB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cabAirFilterTB)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button engAirFilter;
+        private System.Windows.Forms.Button cabAirFilter;
+        private System.Windows.Forms.Label engAirFilterLB;
+        private System.Windows.Forms.Label cabAirFilterLB;
+        private System.Windows.Forms.TrackBar engAirFilterTB;
+        private System.Windows.Forms.TrackBar cabAirFilterTB;
+        private System.Windows.Forms.Button resetBTTN;
+        private System.Windows.Forms.Button exitBTTN;
+        private System.Windows.Forms.Label engAirFilterTbLb;
+        private System.Windows.Forms.Label cabAirFilterTbLb;
     }
 }
--- MaintenanceTracker/AirFilterOptionsForm.cs
+++ MaintenanceTracker/AirFilterOptionsForm.cs
@@ -1,23 +1,199 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using MaintenanceTracker.Properties;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MaintenanceTracker
 {
+
     public partial class AirFilterOptionsForm : System.Windows.Forms.Form
     {
+
+        //Variables
+        private int mls = 15000;
+        private int mx = 30000;
+        private int eMX = 30000;
+        private int cMX = 30000;
+        private int eCount = 0;
+        private int cCount = 0;
+        private int oneFourth = 0;
+        private int oneHalf = 0;
+        private int threeFourth = 0;
+        private Color primaryColor = Color.FromArgb(0, 188, 212);
+        private Color secondaryColor = Color.FromArgb(149, 117, 205);
+        
         public AirFilterOptionsForm()
         {
             InitializeComponent();
 
             //Center form on the screen.
             this.StartPosition = FormStartPosition.CenterScreen;
+
+            //Calculations
+            oneFourth = mx / 4;
+            oneHalf = mx / 2;
+            threeFourth = oneFourth + oneHalf;
+
+            //OnLoad Display
+            this.BackColor = primaryColor;
+
+            //OnLoad Methods
+            EngFilterStatusColor(mls);
+            CabFilterStatusColor(mls);
+
+            //Engine & Cabin Air Filter Text
+            engAirFilterLB.Font = new Font(engAirFilterLB.Font.FontFamily, 10);
+            engAirFilterLB.Text = "Replace the engine air filter every \n" +
+                "15,000-30,000 miles, or once a \n" +
+                "year. You are currently at: \n" +
+                "" + string.Format("{0:n0}", mls) + "/" + string.Format("{0:n0}", mx) + " miles.";
+            cabAirFilterLB.Font = new Font(cabAirFilterLB.Font.FontFamily, 10);
+            cabAirFilterLB.Text = "Replace the cabin air filter every \n" +
+                "15,000-25,000 miles, or once a \n" +
+                "year. You are currently at: \n" +
+                "" + string.Format("{0:n0}", mls) + "/" + string.Format("{0:n0}", mx) + " miles.";
+
+            //Track bar
+            engAirFilterTB.Visible = false;
+            cabAirFilterTB.Visible = false;
+
+            //Track barLabel
+            engAirFilterTbLb.Visible = false;
+            engAirFilterTbLb.Text = mx.ToString();
+            cabAirFilterTbLb.Visible = false;
+            cabAirFilterTbLb.Text = mx.ToString();
+
+        }
+        
+        //Engine's Filter Condition
+        private void EngFilterStatusColor(int M)
+        {
+            //Define the Engine Air Filter Background color
+            //depending on the MPG or amount of months it
+            //has been used.
+            if (M <= oneFourth)
+            {
+                engAirFilter.BackColor = Color.Green;
+            }
+            else if (M > oneFourth && M <= oneHalf)
+            {
+                engAirFilter.BackColor = Color.GreenYellow;
+            }
+            else if (M > oneHalf && M <= threeFourth)
+            {
+                engAirFilter.BackColor = Color.Yellow;
+            }
+            else
+            {
+                engAirFilter.BackColor = Color.Red;
+            }
+        }
+        
+        //Engine's Filter Condition
+        private void CabFilterStatusColor(int M)
+        {
+            //Define the Cabin Air Filter Background color
+            //depending on the MPG or amount of months it
+            //has been used.
+            if (M <= oneFourth)
+            {
+                cabAirFilter.BackColor = Color.Green;
+
+            }
+            else if (M > oneFourth && M <= oneHalf)
+            {
+                cabAirFilter.BackColor = Color.GreenYellow;
+            }
+            else if (M > oneHalf && M <= threeFourth)
+            {
+                cabAirFilter.BackColor = Color.Yellow;
+            }
+            else
+            {
+                cabAirFilter.BackColor = Color.Red;
+            }
+        }
+
+        private void EngAirFilter_Click(object sender, EventArgs e)
+        {
+            if (eCount == 0)
+            {
+                engAirFilter.Text = "";
+                engAirFilter.BackColor = Color.White;
+                engAirFilter.Image = Resources.X120;
+                engAirFilterTB.Visible = true;
+                engAirFilterTbLb.Visible = true;
+                eCount++;
+            }
+            else if (eCount == 1)
+            {
+                engAirFilter.Text = "Engine Air Filter";
+                EngFilterStatusColor(mls);
+                engAirFilter.Image = null;
+
+                // Set up the TrackBar.
+                engAirFilterTB.Visible = false;
+                engAirFilterTbLb.Visible = false;
+                engAirFilterTB.Scroll += new System.EventHandler(EngAirFilterSB_Scroll);
+                engAirFilterTB.Maximum = mx;
+                engAirFilterTB.TickFrequency = 5000;
+                engAirFilterTB.LargeChange = 10000;
+                engAirFilterTB.SmallChange = 5000;
+                eCount--;
+            }
+        }
+
+        private void CabAirFilter_Click(object sender, EventArgs e)
+        {
+            if (cCount == 0)
+            {
+                cabAirFilter.Text = "";
+                cabAirFilter.BackColor = Color.White;
+                cabAirFilter.Image = Resources.X120;
+                cabAirFilterTB.Visible = true;
+                cabAirFilterTbLb.Visible = true;
+                cCount++;
+            }
+            else if (cCount == 1)
+            {
+                cabAirFilter.Text = "Cabin Air Filter";
+                CabFilterStatusColor(mls);
+                cabAirFilter.Image = null;
+
+                // Set up the TrackBar.
+                cabAirFilterTB.Visible = false;
+                cabAirFilterTbLb.Visible = false;
+                cabAirFilterTB.Scroll += new System.EventHandler(CabAirFilterSB_Scroll);
+                cabAirFilterTB.Maximum = mx;
+                cabAirFilterTB.TickFrequency = 5000;
+                cabAirFilterTB.LargeChange = 10000;
+                cabAirFilterTB.SmallChange = 5000;
+                cCount--;
+            }
+        }
+
+
+
+        private void EngAirFilterSB_Scroll(object sender, System.EventArgs e)
+        {
+            //Display the trackbar value in the text box.
+            engAirFilterTbLb.Text = "" + engAirFilterTB.Value;
+        }
+
+        private void CabAirFilterSB_Scroll(object sender, System.EventArgs e)
+        {
+            //Display the trackbar value in the text box.
+            cabAirFilterTbLb.Text = "" + cabAirFilterTB.Value;
+        }
+
+        private void ExitBTTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ResetBTTN_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
